ad0d2dbbff879770d8372a761859b7fa179eb452
bryanjuarez <blimon@aliquammed.com>
2025-11-29 12:43:58 -0800

    cambios back

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from app import create_app
 from app.migrate import run_migrations
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 run_migrations()   # <--- aplica migraciones automáticas y seed
 
 app = create_app()
+
+# 🔥 Arregla el HTTPS detrás de nginx
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+# 🔥 Fuerza Flask a generar y entender HTTPS
+app.config["PREFERRED_URL_SCHEME"] = "https"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
